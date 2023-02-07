842c109c2735978817d11b312f9beeaebf37aace
ustinovilya <ilyaustinov.front@gmail.com>
2023-02-07 12:36:05 +0300

    refactor: fix view comments

--- js/index.js
+++ js/index.js
@@ -8,20 +8,31 @@ const state = {
 const buttonModalSignIn = document.querySelector(".button-sign-in");
 const modal = document.querySelector(".modal");
 const buttonModalClosed = document.querySelector(".modal__form-close");
+const overlayModal = document.querySelector(".modal__overlay");
 const inputEmail = document.querySelector("#email");
 const inputPassword = document.querySelector("#password");
 
-const handleButtonClick = () => {
-  modal.classList.replace("modal--closed", "modal--opened");
+const handleCloseodalByEscape = (e) => {
+  if (e.key === "Escape") {
+    handleButtonModalCloseClick();
+  }
 };
 
-const handleButtonModalCloseClick = () => {
-  modal.classList.replace("modal--opened", "modal--closed");
+const handleButtonClick = () => {
+  modal.classList.replace("modal--closed", "modal--opened");
+  document.addEventListener("keydown", handleCloseodalByEscape);
 };
+
+function handleButtonModalCloseClick() {
+  modal.classList.replace("modal--opened", "modal--closed");
+  document.removeEventListener("keydown", handleCloseodalByEscape);
+}
 
 buttonModalSignIn.addEventListener("click", handleButtonClick);
 
 buttonModalClosed.addEventListener("click", handleButtonModalCloseClick);
+
+overlayModal.addEventListener("click", handleButtonModalCloseClick);
 
 const handleChangeState = (e) => {
   const { value, name } = e.target;
